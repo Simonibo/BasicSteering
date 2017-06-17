6b155d1b8b509f8383b79aa2f14c1f1e54f02421
Cem-Marvin von Hagen <M.vonHagen@macbook-pro-v-hagen.local>
2017-06-17 10:52:49 +0200

    Bus drehen

--- BasicSteering/ViewController.swift
+++ BasicSteering/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     var curDir = 0
     
+    @IBOutlet var busbutton: UIButton!
     @IBOutlet var rbutton: UIButton!
     @IBOutlet var lbutton: UIButton!
     @IBOutlet var vbutton: UIButton!
@@ -21,8 +22,29 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    @IBAction func stopclicked(_ sender: Any) {
+        curDir = 0
+        if let image = UIImage(named: "rL.png") {
+            rbutton.setImage(image, for: [])
+        }
+        if let image = UIImage(named: "vL.png") {
+            vbutton.setImage(image, for: [])
+        }
+        if let image = UIImage(named: "lL.png") {
+            lbutton.setImage(image, for: [])
+        }
+        sendSignal(url: URL(string: "https://io.adafruit.com/api/groups/myvalues/send.json?x-aio-key=e03b7fcf6e7c41cab6fc57db7b2102ec&curdir=" + String(curDir))!)
+    }
+    
     @IBAction func busclicked(_ sender: Any) {
-        
+        if forward, let image = UIImage(named: "backwardlkw.gif") {
+            busbutton.setImage(image, for: [])
+            forward = false
+        } else if !forward, let imagen = UIImage(named: "LKW.gif") {
+            busbutton.setImage(imagen, for: [])
+            forward = true
+        }
+        sendSignal(url: URL(string: "https://io.adafruit.com/api/groups/myvalues/send.json?x-aio-key=e03b7fcf6e7c41cab6fc57db7b2102ec&forward=" + String(forward ? 1 : 0))!)
     }
     
     @IBAction func rightcklicked(_ sender: Any) {
@@ -36,6 +58,7 @@ class ViewController: UIViewController {
         if let image = UIImage(named: "lL.png") {
             lbutton.setImage(image, for: [])
         }
+        sendSignal(url: URL(string: "https://io.adafruit.com/api/groups/myvalues/send.json?x-aio-key=e03b7fcf6e7c41cab6fc57db7b2102ec&curdir=" + String(curDir))!)
     }
     
     @IBAction func leftClicked(_ sender: Any) {
@@ -49,6 +72,7 @@ class ViewController: UIViewController {
         if let image = UIImage(named: "lV.png") {
             lbutton.setImage(image, for: [])
         }
+        sendSignal(url: URL(string: "https://io.adafruit.com/api/groups/myvalues/send.json?x-aio-key=e03b7fcf6e7c41cab6fc57db7b2102ec&curdir=" + String(curDir))!)
     }
     
     @IBAction func aheadClicked(_ sender: Any) {
@@ -62,6 +86,17 @@ class ViewController: UIViewController {
         if let image = UIImage(named: "lL.png") {
             lbutton.setImage(image, for: [])
         }
+        sendSignal(url: URL(string: "https://io.adafruit.com/api/groups/myvalues/send.json?x-aio-key=e03b7fcf6e7c41cab6fc57db7b2102ec&curdir=" + String(curDir))!)
+    }
+    
+    func sendSignal(url: URL) {
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        let session = URLSession.shared
+        
+        session.dataTask(with: request) {data, response, err in
+            print("senden...")
+            }.resume()
     }
 }
 
