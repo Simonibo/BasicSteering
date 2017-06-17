6d882ed050e7d80c98e1d54b34b6690997d109d8
Cem-Marvin von Hagen <M.vonHagen@macbook-pro-v-hagen.local>
2017-06-17 10:30:03 +0200

    buttons working

--- BasicSteering/ViewController.swift
+++ BasicSteering/ViewController.swift
@@ -11,23 +11,53 @@ import UIKit
 class ViewController: UIViewController {
     var curDir = 0
     
+    @IBOutlet var rbutton: UIButton!
+    @IBOutlet var lbutton: UIButton!
+    @IBOutlet var vbutton: UIButton!
+    var forward = true
+    
     @IBAction func busclicked(_ sender: Any) {
+        
     }
     
     @IBAction func rightcklicked(_ sender: Any) {
         curDir = 3
+        if let image = UIImage(named: "rV.png") {
+            rbutton.setImage(image, for: [])
+        }
+        if let image = UIImage(named: "vL.png") {
+            vbutton.setImage(image, for: [])
+        }
+        if let image = UIImage(named: "lL.png") {
+            lbutton.setImage(image, for: [])
+        }
     }
     
     @IBAction func leftClicked(_ sender: Any) {
         curDir = 1
+        if let image = UIImage(named: "rL.png") {
+            rbutton.setImage(image, for: [])
+        }
+        if let image = UIImage(named: "vL.png") {
+            vbutton.setImage(image, for: [])
+        }
+        if let image = UIImage(named: "lV.png") {
+            lbutton.setImage(image, for: [])
+        }
     }
     
     @IBAction func aheadClicked(_ sender: Any) {
         curDir = 2
+        if let image = UIImage(named: "rL.png") {
+            rbutton.setImage(image, for: [])
+        }
+        if let image = UIImage(named: "vV.png") {
+            vbutton.setImage(image, for: [])
+        }
+        if let image = UIImage(named: "lL.png") {
+            lbutton.setImage(image, for: [])
+        }
     }
-    
-    @IBOutlet weak var leftButton: UIButton!
-    var forward = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +65,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func fbchanged(_ sender: Any) {
+        /*
         var url : URL
         if fb.selectedSegmentIndex == 0 {
             forward = true
@@ -52,6 +83,7 @@ class ViewController: UIViewController {
             print("Entered the completionHandler")
             }.resume()
         print(forward)
+         */
     }
 }
 
